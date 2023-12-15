a5deb3b2f56e84de4f845b221c97c09f87e98671
Eugen <epopescuu@yahoo.it>
2023-12-15 16:33:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HelloWorld PHP</title>
+        <link rel="shortcut icon" href="/img/favicon.png">
+
+        <!-- creazione di una variabile -->
+        <?php $nome = "Hello World" ?>
+        <?php $w = "buon weekend" ?>
+    </head>
+    <body>
+        <h1>
+            <?php echo $nome." PHP" ?>
+        </h1>
+        
+        <h2>
+            <?php echo " $w con php " ?>
+        </h2>
+    </body>
+</html>
